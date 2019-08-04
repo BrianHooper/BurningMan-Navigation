5e0cb4c04ca36f87d318925d16f547b3d99049ab
Brian Hooper <brian_hooper@msn.com>
2019-08-04 10:53:37 -0700

    gps

--- get_gps_data.py
+++ get_gps_data.py
@@ -15,7 +15,7 @@ def execute_command(command_string):
 
 
 def get_coord():
-    raw_data = execute_command("gpspipe -w | head -n 5 | tail -n 1")
+    raw_data = execute_command("gpspipe -w | head -n 10 | tail -n 1")
     try:
         json_data = json.loads(raw_data)
         if "lat" in json_data and "lon" in json_data:
@@ -28,23 +28,25 @@ def get_coord():
                     lon = float(lon)
                 except ValueError:
                     print("Error converting values to float: " + str(lat) + ", " + str(lon), file=sys.stderr)
-                    exit(-1)
+                    return None
 
             return str(round(lat, 4)), str(round(lon, 4))
 
         else:
             print("Error: Keys not in string " + raw_data, file=sys.stderr)
-            exit(-1)
+            return None
     except json.JSONDecodeError:
         print("Error decoding string " + raw_data, file=sys.stderr)
-        exit(-1)
+        return None
 
 
 while True:
-    sleep(10)
+    sleep(3)
     print("Getting coordinates")
-    lat, lon = get_coord()
-    with open("config/coordinate", "w") as outfile:
-        outfile.write(lat + "\n")
-        outfile.write(lon + "\n")
-    print("Recieved %s, %s" % (lat, lon))
+    gps = get_coord()
+    if gps is not None:
+        lat, lon = gps
+#       with open("config/coordinate", "w") as outfile:
+#           outfile.write(lat + "\n")
+#           outfile.write(lon + "\n")
+        print("Recieved %s, %s" % (lat, lon))
